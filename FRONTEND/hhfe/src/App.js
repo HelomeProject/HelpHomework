db2840c2e8d7331315363c03f42cf1a760f9cc95
NamSeonWoong <wun1107@gmail.com>
2020-05-22 16:24:12 +0900

    S02P31C110-44

--- FRONTEND/hhfe/src/App.js
+++ FRONTEND/hhfe/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import MyAppBar from './components/mainpage/MyAppBar'
-
+// import MyAppBar from './components/mainpage/MyAppBar'
+import Signin from './components/signin/signin'
+import Signup from './components/signup/signup'
 const App = () => {
   return (
     <div className="App">
-      <MyAppBar />
+      <Signup/>
     </div>
   );
 }
