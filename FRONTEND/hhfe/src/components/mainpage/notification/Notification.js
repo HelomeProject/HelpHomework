48e47036e8f3c744331b153e7015f5488c4f8a10
GoPro1147 <cooth1147@gmail.com>
2020-05-27 14:38:04 +0900

    S02P31C110-47 [+] Add Calendar

--- FRONTEND/hhfe/src/components/mainpage/notification/Notification.js
+++ FRONTEND/hhfe/src/components/mainpage/notification/Notification.js
@@ -1,9 +1,16 @@
-import React, { createRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { IconButton, Grid, Typography } from '@material-ui/core';
+import { ArrowBackIos, ArrowForwardIos } from '@material-ui/icons';
+import useStyle from './NotificationCSS'
+
 import Calendar from '@toast-ui/react-calendar';
 import 'tui-calendar/dist/tui-calendar.css';
-import Grid from '@material-ui/core/Grid'
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
+
+const themeConfig = {
+    'month.dayname.textAlign': 'center'
+}
 
 const calendarOptions = {
     view: 'month',
@@ -13,56 +20,59 @@ const calendarOptions = {
     useDetailPopup: true,
     useCreationPopup: true,
     usageStatistics: false,
-    template: {
-        milestone(schedule) {
-            return `<span style="color:#fff;background-color: ${schedule.bgColor};">${
-                schedule.title
-                }</span>`;
-        },
-        milestoneTitle() {
-            return 'Milestone';
-        },
-        allday(schedule) {
-            return `${schedule.title}<i class="fa fa-refresh"></i>`;
-        },
-        alldayTitle() {
-            return 'All Day';
-        },
 
-
-    },
     month: {
         daynames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'],
-        startDayOfWeek: 0,
-        narrowWeekend: true
-
-    }
+        workweek: true
+    },
+    theme: themeConfig
 }
 
 
 const Notification = () => {
-    const calendarRef = createRef();
-    console.log(calendarRef)
+    const classes = useStyle()
+    const [month, setMonth] = useState(null)
+    const [year, setYear] = useState(null)
+    const calendarRef = useRef(null);
+    // const calendarRef = createRef();
+
+
+    useEffect(() => {
+        const changeDate = () => {
+            const calendarInstance = calendarRef.current.getInstance();
+            setMonth(calendarInstance.getDate().toDate().getMonth())
+            setYear(calendarInstance.getDate().toDate().getFullYear())
+        }
+        changeDate()
+    })
+
     const handleClickNextButton = () => {
         const calendarInstance = calendarRef.current.getInstance();
-        console.log(calendarInstance)
         calendarInstance.next();
+        setMonth(calendarInstance.getDate().toDate().getMonth())
+        setYear(calendarInstance.getDate().toDate().getFullYear())
     };
-    const handleClickButton = () => {
-        calendarRef.current.getRootElement().classList.add('calendar-root');
+    const handleClickPrevButton = () => {
+        const calendarInstance = calendarRef.current.getInstance();
+        calendarInstance.prev();
+        setMonth(calendarInstance.getDate().toDate().getMonth())
+        setYear(calendarInstance.getDate().toDate().getFullYear())
     };
 
     return (
         <>
-            <Grid item xs={12} sm={12} md={9}>
-                <button onClick={handleClickButton}>Click!</button>
-                <button onClick={handleClickNextButton}>Go next!</button>
+            <Grid item xs={12} sm={12} md={8}>
+                <Grid container justify='space-between' alignItems='center'>
+                    <IconButton className={classes.calendarTitle} onClick={handleClickPrevButton}><ArrowBackIos /></IconButton>
+                    <Typography className={classes.calendarTitle}>{year}년 {month}월</Typography>
+                    <IconButton className={classes.calendarTitle} onClick={handleClickNextButton}><ArrowForwardIos /></IconButton>
+                </Grid>
                 <Calendar
                     {...calendarOptions}
                     ref={calendarRef}
                 />
             </Grid>
-            <Grid item xs={12} sm={12} md={3}>
+            <Grid item xs={12} sm={12} md={4}>
 
             </Grid>
         </>
