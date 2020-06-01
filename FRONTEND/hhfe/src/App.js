84ca569a54d7b252691447c126b78f860284df74
GoPro1147 <cooth1147@gmail.com>
2020-06-01 10:46:26 +0900

    S02P31C110-44 [+] Add react cookie

--- FRONTEND/hhfe/src/App.js
+++ FRONTEND/hhfe/src/App.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Route } from "react-router-dom";
+import { withCookies, useCookies } from 'react-cookie';
 
 import MyAppBar from './components/mainpage/MyAppBar'
+import Login from './components/account/Login'
+import Register from './components/account/Register'
+
 
 const App = () => {
-  return (
-    <div className="App">
-      <MyAppBar />
-    </div>
-  );
-}
 
-export default App;
+  const [cookies, removeCookie] = useCookies(['user']);
+  const [hasCookie, setHasCookie] = useState(false);
+
+  useEffect(() => {
+    if (cookies.user && cookies.user !== 'undefined') {
+      setHasCookie(true);
+    }
+  }, [cookies]);
+
+  return (
+    <>
+      <Route
+        exact path="/"
+        render={routerProps => {
+          return (
+            <Login
+              {...routerProps}
+              hasCookie={hasCookie}
+              setHasCookie={setHasCookie}
+            />
+          );
+        }}
+      />
+      <Route
+        exact path="/register"
+        component={Register}
+      />
+      <Route
+        exact path="/main"
+        render={routerProps => {
+          return (
+            <MyAppBar
+              {...routerProps}
+              setHasCookie={setHasCookie}
+              removeCookie={() => {
+                removeCookie('user');
+                setHasCookie(false);
+              }}
+            />
+          );
+        }}
+      />
+    </>
+  );
+};
+
+export default withCookies(App);
