285a0f7cd8dc30e86321ae174267c10567aae13c
cdh3261 <cdh3261@naver.com>
2020-06-01 14:58:57 +0900

    S02P31C110-19 [*] Define Model

--- ML/train.py
+++ ML/train.py
@@ -11,6 +11,23 @@ img_size = (45,45)
 
 image_path = './data_45/'
 
+model = Sequential()
+
+model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(45, 45, 1)))  # 필터개수, 필터 사이즈, activation function , input_shape=(가로크기,세로크기,차원)
+model.add(Conv2D(128, (3, 3), activation='relu'))
+# model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+# model.add(BatchNormalization()) # 정규화
+model.add(Flatten()) # 1차원으로 펴준다.
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes, activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',    # mean square error, mean absolute error 등 사용 가능
+              optimizer='adam',                                       # 최적화 알고리즘, sgd, adam 등등
+              metrics=['accuracy'])
+
 image_gen = ImageDataGenerator(rescale=1./255, validation_split=0.2)
 
 train_gen = image_gen.flow_from_directory(
