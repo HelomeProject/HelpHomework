4f48bb0607e87c60bce9bd2315f01fd6205f1a33
고태환 <cooth1147@gmail.com>
2020-06-07 12:00:00 +0900

    S02P31C110-72[+] 새로고침 이후에 토큰을이용해 유저 정보 가져오기

--- FRONTEND/hhfe/src/App.js
+++ FRONTEND/hhfe/src/App.js
@@ -1,30 +1,49 @@
-
 import React, { useState, useEffect } from 'react';
 import { Route, Redirect, Switch } from "react-router-dom";
 import { withCookies, useCookies } from 'react-cookie';
+import axios from 'axios'
 
 import MyAppBar from './components/mainpage/MyAppBar'
 import Login from './components/account/Login'
 import Register from './components/account/Register'
+
+
 const App = () => {
 
-  const [cookies, removeCookie] = useCookies(['user']);
+  const [cookies, setCookie, removeCookie] = useCookies(['token']);
   const [hasCookie, setHasCookie] = useState(false);
   const [mode, setMode] = useState(0)
+  const [userInfo, setUserInfo] = useState({})
 
   useEffect(() => {
-    if (cookies.user && cookies.user !== 'undefined') {
-      console.log(cookies)
+    const reloaduser = (idx, config) => {
+      axios.get('http://k02c1101.p.ssafy.io:9090/api/member/users/' + idx, config)
+      .then((res) => {
+        setUserInfo(res.data.Member)
+        setMode(parseInt(res.data.Member.isteacher))
+      })
+      .catch((e) => console.log(e))
+    }
+    if (cookies.token && cookies.token !== 'undefined') {
+
+      const config = {
+        headers : {'Authorization':cookies.token},
+      }
+
+      reloaduser(cookies.memberIdx, config)
       setHasCookie(true);
     }
   }, [cookies]);
+
   const logout = () => {
-    removeCookie('user');
+    removeCookie('token', {path:'/', expires: 0});
+    removeCookie('memberIdx', {path:'/', expires: 0});
     setHasCookie(false);
   }
+
   return (
     <>
-      {/* {!hasCookie ? <Redirect to="/" /> : <Redirect to='/main' />} */}
+      {!hasCookie ? <Redirect to="/" /> : <Redirect to='/main' />}
       <Switch>
         <Route
           exact path="/"
@@ -33,8 +52,10 @@ const App = () => {
               <Login
                 {...routerProps}
                 hasCookie={hasCookie}
+                setCookie={setCookie}
                 setHasCookie={setHasCookie}
                 setMode={setMode}
+                setUserInfo={setUserInfo}
               />
             );
           }}
@@ -49,8 +70,9 @@ const App = () => {
             return (
               <MyAppBar
                 mode={mode}
+                userInfo={userInfo}
                 setHasCookie={setHasCookie}
-                removeCookie={logout}
+                logout={logout}
               />
             );
           }}
