1b96f18ce5b46571e3b788b81b53057624fdeb4f
Ko <cooth1147@gmail.com>
2020-06-08 06:19:03 +0900

    S02P31C110-47 [+] 공지사항 저장용 경로

--- MLSERVER/MLCal/urls.py
+++ MLSERVER/MLCal/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('calc/', views.calc, name="calc")
+    path('calc/', views.calc, name="calc"),
+    path('addnoti/', views.savenotification, name="addnoti")
 ]
