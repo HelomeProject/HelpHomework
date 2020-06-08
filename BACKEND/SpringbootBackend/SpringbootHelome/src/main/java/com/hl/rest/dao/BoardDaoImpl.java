108f2513df87b487bdbf25b28050fecc75761b5d
haeun <sj05publu@naver.com>
2020-06-08 10:12:48 +0900

    S02P31C110-90 [-] BoardService에서 homework 관련 로직 삭제

--- BACKEND/SpringbootBackend/SpringbootHelome/src/main/java/com/hl/rest/dao/BoardDaoImpl.java
+++ BACKEND/SpringbootBackend/SpringbootHelome/src/main/java/com/hl/rest/dao/BoardDaoImpl.java
@@ -17,43 +17,6 @@ public class BoardDaoImpl implements IBoardDao {
 	SqlSession session;
 
 	@Override
-	public void insertHomework(Homework_old homework) {
-		session.insert("board.insertHomework", homework);
-	}
-
-	@Override
-	public int getHomeListSize(String grade, String classnum) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("grade", Integer.parseInt(grade));
-		map.put("classnum", Integer.parseInt(classnum));
-		return session.selectOne("board.getHomeworkListSize", map);
-	}
-
-	@Override
-	public List<Homework_old> getHomeworkList(int startlist, int listsize, String grade, String classnum) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("startlist", startlist);
-		map.put("listsize", listsize);
-		map.put("grade", Integer.parseInt(grade));
-		map.put("classnum", Integer.parseInt(classnum));
-		return session.selectList("board.getHomeworkList", map);
-	}
-
-	@Override
-	public int getHomeListSize(String memberIdx) {
-		return session.selectOne("board.getHomeworkSize", Integer.parseInt(memberIdx));
-	}
-
-	@Override
-	public List<Homework_old> getHomeworkList(int startlist, int listsize, String memberIdx) {
-		Map<String, Object> map = new HashMap<>();
-		map.put("startlist", startlist);
-		map.put("listsize", listsize);
-		map.put("memberIdx", Integer.parseInt(memberIdx));
-		return session.selectList("board.getMyHomeworkList", map);
-	}
-
-	@Override
 	public Notice getNotice(String noticeIdx) {
 		return session.selectOne("board.getNotice", Integer.parseInt(noticeIdx));
 	}
--- BACKEND/SpringbootBackend/SpringbootHelome/src/main/java/com/hl/rest/service/IBoardService.java
+++ BACKEND/SpringbootBackend/SpringbootHelome/src/main/java/com/hl/rest/service/IBoardService.java
@@ -6,12 +6,6 @@ import com.hl.rest.vo.Homework_old;
 import com.hl.rest.vo.Notice;
 
 public interface IBoardService {
-	public void insertHomework(Homework_old homework);
-	public int getHomeListSize(String grade, String classnum);
-	public int getHomeListSize(String memberIdx);
-	public List<Homework_old> getHomeworkList(int startlist, int listsize, String grade, String classnum);
-	public List<Homework_old> getHomeworkList(int startlist, int listsize, String memberIdx);
-	
 	public Notice getNotice(String noticeIdx);
 	public void createNotice(Notice notice);
 	public List<Notice> getNoticeList();
