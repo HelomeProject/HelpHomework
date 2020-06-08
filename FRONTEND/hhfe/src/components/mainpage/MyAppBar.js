fcfda2390dbc6e6cc34bf7a59b0672136829069c
Ko <cooth1147@gmail.com>
2020-06-08 14:57:52 +0900

    S02P31C110-61 [+] 선생님 or 학생에 따라 보여주는 값 다름

--- FRONTEND/hhfe/src/components/mainpage/MyAppBar.js
+++ FRONTEND/hhfe/src/components/mainpage/MyAppBar.js
@@ -8,37 +8,50 @@ import Button from '@material-ui/core/Button';
 import useStyles from './MyAppBarCSS'
 import LeftDrawer from './LeftDrawer'
 import clsx from 'clsx';
+import { Redirect } from "react-router-dom";
 
-const MyAppBar = ({ mode, logout, userInfo }) => {
+
+const MyAppBar = ({ mode, logout, userInfo, hasCookie }) => {
     const classes = useStyles();
     const [open, setOpen] = useState(true);
 
     const handleDrawerToggle = () => {
         setOpen(!open);
     };
-    
+
     return (
-        <div className={classes.root}>
-            <AppBar position="static"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}>
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        className={classes.menuButton}
-                        color="inherit" aria-label="menu"
-                        onClick={handleDrawerToggle}>
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        {userInfo.username}
-                    </Typography>
-                    <Button color="inherit" onClick={logout}>logout</Button>
-                </Toolbar>
-            </AppBar>
-            <LeftDrawer open={open} mode={mode} userInfo={userInfo} />
-        </div>
+        <>
+            {!hasCookie ? <Redirect to='/' /> :
+                < div className={classes.root} >
+                    <AppBar position="static"
+                        className={clsx(classes.appBar, {
+                            [classes.appBarShift]: open,
+                        })}>
+                        <Toolbar>
+                            <IconButton
+                                edge="start"
+                                className={classes.menuButton}
+                                color="inherit" aria-label="menu"
+                                onClick={handleDrawerToggle}>
+                                <MenuIcon />
+                            </IconButton>
+                            {mode ?
+                                <Typography variant="h6" className={classes.title}>
+                                    {userInfo.username} 선생님 안녕하세요
+                                </Typography>
+                                :
+                                <Typography variant="h6" className={classes.title}>
+                                    {userInfo.username} 안녕!
+                                </Typography>
+                            }
+
+                            <Button color="inherit" onClick={logout}>logout</Button>
+                        </Toolbar>
+                    </AppBar>
+                    <LeftDrawer open={open} mode={mode} userInfo={userInfo} />
+                </div >
+            }
+        </>
     );
 }
 
