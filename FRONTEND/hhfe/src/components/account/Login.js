59051a3b371e8ed93e0dbefa0060f118b0da0f37
Ko <cooth1147@gmail.com>
2020-06-08 14:54:28 +0900

    S02P31C110-44 [*] 로그인 성공 시 main으로 이동

--- FRONTEND/hhfe/src/components/account/Login.js
+++ FRONTEND/hhfe/src/components/account/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Avatar, Button } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { TextField, FormControlLabel, Checkbox, Link } from '@material-ui/core';
+import { TextField, Link } from '@material-ui/core';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
@@ -10,20 +10,20 @@ import Typography from '@material-ui/core/Typography';
 import useStyles from './LoginCSS'
 import axios from 'axios'
 import sha256 from './encryptSHA256';
-
+import { Redirect } from "react-router-dom";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'© '}
       <Link color="inherit" href="">
-        홈런(Home-Learn)~!!
+        홈런(Home-Learn)
       </Link>{' '}
     </Typography>
   );
 }
 
-const Login = ({ setMode, setCookie, setHasCookie, setUserInfo }) => {
+const Login = ({ setMode, setCookie, hasCookie, setHasCookie, setUserInfo }) => {
   const classes = useStyles();
   const [loginInfo, setLoginInfo] = useState({
     username: "",
@@ -61,7 +61,7 @@ const Login = ({ setMode, setCookie, setHasCookie, setUserInfo }) => {
       const response = await loginApi(loginInfo);
       if (response.status === 200) {
         setCookie('memberIdx', response.data.memberIdx)
-        setCookie('token',response.data.token)
+        setCookie('token', response.data.token)
         setHasCookie(true);
         setUserInfo(response.data)
         setMode(parseInt(response.data.isteacher))
@@ -82,72 +82,68 @@ const Login = ({ setMode, setCookie, setHasCookie, setUserInfo }) => {
 
   return (
     <>
-      <Grid container component="main" className={classes.root}>
-        <CssBaseline />
-        <Grid item xs={false} sm={4} md={7} className={classes.image} />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-          <div className={classes.paper}>
-            <Avatar className={classes.avatar}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              로 그 인
+      {
+        hasCookie ? <Redirect to='/main' /> :
+          <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+              <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  로 그 인
           </Typography>
-            <form className={classes.form} onSubmit={handleSubmit} method="POST">
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="username"
-                autoComplete="email"
-                autoFocus
-                onChange={handleChange}
-              />
-              <TextField
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                onChange={handleChange}
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="회원정보 기억하기"
-              />
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-              >
-                로그인
+                <form className={classes.form} onSubmit={handleSubmit} method="POST">
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="username"
+                    autoComplete="email"
+                    autoFocus
+                    onChange={handleChange}
+                  />
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={handleChange}
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                  >
+                    로그인
             </Button>
-              <Grid container>
-                <Grid item>
-                  <Link href="/register" variant="body2">
-                    {"아직회원이 아니세요??"}
-                  </Link>
-                </Grid>
-              </Grid>
-              <Box mt={5}>
-                <Copyright />
-              </Box>
-            </form>
-          </div>
-        </Grid>
-      </Grid>
-  )
-}
+                  <Grid container>
+                    <Grid item>
+                      <Link href="/register" variant="body2">
+                        {"아직회원이 아니세요??"}
+                      </Link>
+                    </Grid>
+                  </Grid>
+                  <Box mt={5}>
+                    <Copyright />
+                  </Box>
+                </form>
+              </div>
+            </Grid>
+          </Grid>
+      }
     </>
   );
 }
